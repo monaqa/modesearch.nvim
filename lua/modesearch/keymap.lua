1f003f59c03f83dbcba5e858adc4ad18b65d5c1c
monaqa <cmonaqa@gmail.com>
2023-02-16 23:32:54 +0900

    add keymap.mode.set

--- lua/modesearch/keymap.lua
+++ lua/modesearch/keymap.lua
@@ -7,6 +7,8 @@ local state = require "modesearch.state"
 M.prompt = {}
 M.mode = {}
 
+---@param mode_name string
+---@return string
 function M.prompt.show(mode_name)
     local mode = config.options.modes[mode_name]
     if mode == nil then
@@ -17,6 +19,7 @@ function M.prompt.show(mode_name)
 end
 
 ---@param mode_names string[]
+---@return string
 function M.mode.cycle(mode_names)
     local index = nil
     for i, name in ipairs(mode_names) do
@@ -36,6 +39,19 @@ function M.mode.cycle(mode_names)
         error("Mode '" .. next_mode_name .. "' is not defined.")
     end
     state.current_mode_name = next_mode_name
+    return core.exprstr "<CR>"
+end
+
+---@param mode_name string
+---@return string
+function M.mode.set(mode_name)
+    if state.current_mode_name == mode_name then
+        return ""
+    end
+    if config.options.modes[mode_name] == nil then
+        error("Mode '" .. mode_name .. "' is not defined.")
+    end
+    state.current_mode_name = mode_name
     return core.exprstr "<CR>"
 end
 
